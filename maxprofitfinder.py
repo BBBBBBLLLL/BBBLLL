3cb4f3afc27bf8cd51ee5f04b7ad6a872247fc2c
BBBBBBLLLL <116550137+BBBBBBLLLL@users.noreply.github.com>
2022-10-27 00:32:18 +1100

    Update maxprofitfinder.py

--- maxprofitfinder.py
+++ maxprofitfinder.py
@@ -5,3 +5,17 @@ st.set_page_config(
     page_title="Financial Planning Calculator")
 
 st.title("Financial Planning Calculator")
+
+st.header("**Monthly Income**")
+st.subheader("Salary")
+colAnnualSal, colTax = st.beta_columns(2)
+
+with colAnnualSal:
+    salary = st.number_input("Enter your annual salary($): ", min_value=0.0, format='%f')
+with colTax:
+    tax_rate = st.number_input("Enter your tax rate(%): ", min_value=0.0, format='%f')
+
+tax_rate = tax_rate / 100.0
+salary_after_taxes = salary * (1 - tax_rate)
+monthly_takehome_salary = round(salary_after_taxes / 12.0, 2)
+view rawBudgetApp_part3.py hosted with ❤ by GitHub
